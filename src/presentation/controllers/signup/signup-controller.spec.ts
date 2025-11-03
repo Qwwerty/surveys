11c98d1ec6ce3aee7146a96a508f5ad01b774e1d
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2025-11-03 07:11:21 -0300

    refactor: move validation to its own layer

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -25,7 +25,7 @@ const makeAuthentication = (): Authentication => {
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
-      return null
+      return null as any
     }
   }
   return new ValidationStub()
@@ -93,7 +93,7 @@ describe('SignUp Controller', () => {
       return new Promise((resolve, reject) => reject(new Error()))
     })
     const httpResponse = await sut.handle(makeFakeRequest())
-    expect(httpResponse).toEqual(serverError(new ServerError(null)))
+    expect(httpResponse).toEqual(serverError(new ServerError(null as any)))
   })
 
   test('Should return 200 if valid data is provided', async () => {
