bd7c858582825d989a48f26db94d51d460b0e08c
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2025-11-03 11:14:01 -0300

    feat: add AddSurvey composition

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -3,7 +3,7 @@ import { Controller } from '../../../../presentation/protocols/controller'
 import { makeSignUpValidation } from './signup-validation-factory'
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
 import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
-import { makeLogControllerDecorator } from '../../usecases/decorators/log-controller-decorator-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 
 export const makeSignUpController = (): Controller => {
   const controller = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
